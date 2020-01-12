63cbfeb07b2eef51065ef4e7e3ac3023e7f118bf
Omer <omerkaraman@hotmail.co.uk>
2020-01-12 00:26:40 +0000

    Added sanitizer

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,27 @@
 const router = require("express").Router();
 const Guestbook = require("../model/Guestbook");
 const {guestbookValidation} = require("../validation");
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = (new JSDOM('')).window;
+const DOMPurify = createDOMPurify(window);
 
 
 router.post("/guestbook", async (req, res) => {
-  const {error} = guestbookValidation(req.body);
+  const purifyOptions = {FORBID_ATTR: ["style"], FORBID_TAGS: ['style']};
+  const clean_body = {
+    name: DOMPurify.sanitize(req.body.name, purifyOptions),
+    message: DOMPurify.sanitize(req.body.message, purifyOptions)
+  };
+
+  const {error} = guestbookValidation(clean_body);
   if (error) {
     return res.status(200).json({post_success: false, error: error.details[0].message});
   }
 
   const bookentry = new Guestbook({
-    name: req.body.name,
-    message: req.body.message
+    name: clean_body.name,
+    message: clean_body.message
   });
 
   try {
